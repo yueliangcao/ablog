f6a0ff508d3f39eb4432922b782e9b19aa173f02
哑谜 <yueliangcao@github.com>
2015-02-06 19:50:48 +0800

    update

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/session"
 	_ "github.com/yueliangcao/ablog/routers"
 )
 
-var globalSessions *session.Manager
-
 func main() {
-	globalSessions, _ = session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "", "cookieLifeTime": 3600, "providerConfig": ""}`)
-	go globalSessions.GC()
-
 	beego.Run()
 }
